f3c91afe629b30f361b40acf4cf9b54f279993e7
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-04-25 09:26:22 +0000

    python debugging package used

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from random import randint
+import pdb
 
-answer = randint(1,10)
-while True:
-  print(answer)
-  try:
-    guess = int(input("number"))
-    if guess>0 and guess<11:
-      if guess== answer:
-        print("yayy")
-        break
-    else:
-      print("nooo")
-  except ValueError:
-    print("enter a numer yu bozo")
+def fun(z,m):
+  pdb.set_trace()
+  return z+m
+
+print(fun(1,"lol"))
